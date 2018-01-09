9d72f35ad43acd65fab4bf10cfb1cd9571a47764
Assuliya <t.nesterchuk@mail.ru>
2018-01-09 20:50:00 +0200

    basic setup. all pages working

---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/panthers');
+
+var PantherSchema = new mongoose.Schema({
+ name: String,
+ color: String,
+}, {timestamps: true });
+
+mongoose.model('Panther', PantherSchema);
+var Panther = mongoose.model('Panther')
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, './static')));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  Panther.find({}, function(err, panthers) {
+    console.log(panthers)
+    res.render('index', {panthers: panthers});
+  })
+})
+
+app.get('/panthers/:id', function(req, res) {
+  // Panther.find({'_id': id}, function(err, panthers) {
+  //   console.log(panther)
+    res.render('one');
+  // })
+})
+
+app.get('/panthers/new', function(req, res) {
+  res.render('new');
+})
+
+app.get('/panthers/edit/:id', function(req, res) {
+  // Panther.find({'_id': id}, function(err, panther) {
+  //   console.log(panther)
+    res.render('edit');
+  // })
+})
+
+app.post('/panthers', function(req, res) {
+    console.log("POST DATA", req.body);
+    var panther = new Panther({name: req.body.name, color: req.body.color});
+    dojo.save(function(err) {
+      if(err) {
+        console.log('something went wrong');
+      } else {
+        console.log('successfully added a panther!');
+    res.redirect('/panthers');
+        }
+    })
+})
+
+app.post('/panthers/:id', function(req, res) {
+
+})
+
+app.post('/panthers/destroy/:id', function(req, res) {
+
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
